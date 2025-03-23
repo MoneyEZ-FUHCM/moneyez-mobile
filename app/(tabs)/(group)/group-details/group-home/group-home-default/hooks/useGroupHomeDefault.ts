a4261f47cabfd32524d98a29817c0f201fcf7f04
bomismegg <minhtri28102003@gmail.com>
2025-03-24 06:54:51 +0700

    feat(ui): add withdraw fund request functionality

--- app/(tabs)/(group)/group-details/group-home/group-home-default/hooks/useGroupHomeDefault.ts
+++ app/(tabs)/(group)/group-details/group-home/group-home-default/hooks/useGroupHomeDefault.ts
@@ -1,15 +1,23 @@
 import { PATH_NAME } from "@/helpers/constants/pathname";
 import { setGroupTabHidden } from "@/redux/slices/tabSlice";
 import { router } from "expo-router";
+import { useCallback } from "react";
 import { useDispatch } from "react-redux";
-
 
 const useGroupHomeDefault = () => {
   const dispatch = useDispatch();
+  
   const handleCreateFundRequest = () => {
     dispatch(setGroupTabHidden(true));
     router.push({
       pathname: PATH_NAME.GROUP_HOME.CREATE_FUND_REQUEST as any,
+    });
+  }
+
+  const handleCreateWithdrawRequest = () => {
+    dispatch(setGroupTabHidden(true));
+    router.push({
+      pathname: PATH_NAME.GROUP_HOME.WITHDRAW_FUND_REQUEST as any,
     });
   }
 
@@ -25,15 +33,14 @@ const useGroupHomeDefault = () => {
   }
 
   return {
-    state: {
-
-    },
+    state: {},
     handler: {
       handleCreateFundRequest,
+      handleCreateWithdrawRequest,
       handleFundRemind,
       handleStatistic,
     },
   };
-}
+};
 
 export default useGroupHomeDefault;
